ff3b23c72616ec7b7f36e367c6c67cefec94bd0d
equescodebelike <nep3600@gmail.com>
2022-03-26 11:49:45 +0300

    main build

--- src/task2_2/Task2Window.java
+++ src/task2_2/Task2Window.java
@@ -1,6 +1,6 @@
 package task2_2;
 
-public class Task2Window {
+public class Task2Window { // запуск оконного интерфейса
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
