b5c4af67434f411a6455a47c4f5d3472ff0ee523
equescodebelike <nep3600@gmail.com>
2022-04-17 14:50:09 +0300

    main build

--- src/task2_3/LengthComparator.java
+++ src/task2_3/LengthComparator.java
@@ -2,7 +2,7 @@ package task2_3;
 
 import java.util.Comparator;
 
-public class LengthComparator implements Comparator<String> {
+public class LengthComparator implements Comparator<String> { // для сравнения длины при использовании стандартной библиотеки
     @Override
     public int compare(String x, String y) {
         return Integer.compare(x.length(), y.length());
